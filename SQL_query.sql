e0bdfb2907fd2c64092fb79e824590cae5caf89e
Peter Chow <peterchowacademy@gmail.com>
2021-11-26 17:55:41 +0800

    python package format done

---
+++ SQL_query.sql
@@ -0,0 +1,3 @@
+SELECT * FROM products ORDER BY id LIMIT 5 OFFSET 2 --- select the top 5 results but skip the first top 2 items
+
+INSERT INTO products (name, price, inventory) VALUES ('tortilla', 4, 100); --- insert rows and values
