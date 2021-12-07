698210af2c1e9607196450490db22841c3bb9fdd
Peter Chow <peterchowacademy@gmail.com>
2021-12-07 23:23:11 +0800

    python package format done

--- app/main.py
+++ app/main.py
@@ -110,20 +110,22 @@ def delete_post(id: int):
     #delete the post 
     #find the index in the array that has required ID
     #my_posts.pop(id)
-    index = find_index_post(id)
-    if index == None:
+    cursor.execute ("""DELETE FROM posts WHERE id = %s RETURNING * """, (str(id),))
+    deleted_post = cursor.fetchone()
+    conn.commit()
+
+    if deleted_post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
 
-    my_posts.pop(index)
     return Response(status_code=status.HTTP_204_NO_CONTENT) #204 when you delete sth, you dont want to send data back
 
 @app.put("/posts/{id}")
 def update_post(id: int, post: Post):
-    index = find_index_post(id)
-    if index == None:
+    cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING * """,
+                   (post.title, post.content, post.published, str(id)))
+    updated_post = cursor.fetchone()
+    conn.commit()
+    if updated_post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
-    post_dict = post.dict()
-    post_dict['id'] = id
-    my_posts[index] = post_dict
     # print(post)
-    return {"data": post_dict}
+    return {"data": updated_post}
